c5c7cf058d96d9eab08cf8acfe39b3a724b3fbea
LENGLET <stephane.lenglet@gmail.com>
2019-04-03 13:04:02 +0200

    modif script sh

--- sh/learn.sh
+++ sh/learn.sh
@@ -1,1 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+python3 $1 \
+--image_dir $2 \
+--bottleneck_dir=$3 \
+--how_many_training_steps=$4 \
+--model_dir=$5 \
+--output_graph=$6 \
+--output_labels=$7
+
+
+#exemple : ./learn.sh ../src/fr.wewyse.iatoolbox/retrain.py ../src/resources/tf_files/images/learn ../src/resources/tf_files/bottlenecks 1000 ../src/resources/tf_files/inception ../src/resources/tf_files/retrained_files.pb ../src/resources/tf_files/retrained_labels.txt
--- sh/recognize.sh
+++ sh/recognize.sh
@@ -1,1 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
+python3 $1 $2
+
+#exemple : ./recognize.sh ../src/fr.wewyse.iatoolbox/label.py ../src/resources/tf_files/images/recognize/pic_001.JPEG
